04c3771cb1702b4dabd7f92104a03fa0fe973874
paperfox <paperfox56@gmail.com>
2026-07-19 16:11:43 +0100

    Implement syscall Refactor write to use syscall instead of raw assembly

--- include/unistd.h
+++ include/unistd.h
@@ -11,6 +11,8 @@ _Noreturn void _exit(int __status);
 ssize_t write(int fd, const void *buf, size_t count);
 ssize_t read(int fd, void *buf, size_t count);
 
+long syscall(long number, ...);
+
 __END_DECLS
 
 #endif /* unistd.h */
--- src/unistd/unistd.zig
+++ src/unistd/unistd.zig
@@ -1,21 +1,38 @@
 const root = @import("root");
 
-pub export fn write(fd: c_int, buf: ?*const anyopaque, count: usize) callconv(.c) isize {
+pub export fn syscall(number: c_long, ...) callconv(.c) c_long {
+    var ap = @cVaStart();
+    defer @cVaEnd(&ap);
+
+    const a1 = @cVaArg(&ap, usize);
+    const a2 = @cVaArg(&ap, usize);
+    const a3 = @cVaArg(&ap, usize);
+    const a4 = @cVaArg(&ap, usize);
+    const a5 = @cVaArg(&ap, usize);
+    const a6 = @cVaArg(&ap, usize);
+
     const result = asm volatile (
         \\ syscall
         : [ret] "={rax}" (-> isize),
-        : [number] "{rax}" (@as(usize, 1)),
-          [di] "{rdi}" (@as(usize, @intCast(fd))),
-          [si] "{rsi}" (@as(usize, @intFromPtr(buf))),
-          [dx] "{rdx}" (@as(usize, count)),
-        : .{
-          .rax = true,
-          .rcx = true,
-          .r11 = true,
-          .memory = true,
-        });
+        : [num] "{rax}" (number),
+          [di] "{rdi}" (a1),
+          [si] "{rsi}" (a2),
+          [dx] "{rdx}" (a3),
+          [r10] "{r10}" (a4),
+          [r8] "{r8}" (a5),
+          [r9] "{r9}" (a6),
+        : .{ .rcx = true, .r11 = true, .memory = true });
 
-    return root.errno.negErrno(result);
+    return @intCast(root.errno.negErrno(result));
+}
+
+pub export fn write(fd: c_int, buf: ?*const anyopaque, count: usize) callconv(.c) isize {
+    return @intCast(syscall(
+        1,
+        @as(usize, @intCast(fd)),
+        @intFromPtr(buf),
+        count,
+    ));
 }
 
 pub export fn _exit(status: c_int) callconv(.c) noreturn {
